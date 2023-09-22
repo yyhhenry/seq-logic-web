f326d26635a223e7e133472d4e33e0cc82b16b16
yyhhenry <yyhhenry@foxmail.com>
2023-09-22 13:55:29 +0800

    fix: icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import { FlexCard, HeaderText, PageLayout, SwitchDark } from 'luoluo-vue-components';
 import { useTitle } from '@vueuse/core';
 import websiteName from '@/utils/website-name';
+import { ElImage } from 'element-plus';
 useTitle(websiteName);
 </script>
 
 <template>
   <PageLayout>
     <template #header>
+      <ElImage src="./favicon.png" fit="scale-down" :style="{ height: '3em' }" />
       <HeaderText>{{ websiteName }}</HeaderText>
     </template>
     <template #header-extra>
